ab6bef78e096a1cf36ad6b8a27fcf52c01cda9c7
Jayteare <Jayteare@users.noreply.github.com>
2015-10-10 17:31:12 -0700

    Update ThreadedSearch_Linear.java

--- Big_Oh_Searching_Analysis/ThreadedSearch_Linear.java
+++ Big_Oh_Searching_Analysis/ThreadedSearch_Linear.java
@@ -1,18 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project 
- * Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package hello;
-
 import static hello.Hello.linear_search_full;
 import java.text.DecimalFormat;
 
-/**
- *
- * @author Can
- */
 public class ThreadSearch_Linear implements Runnable {
     
     Hello mainSc = new Hello();
